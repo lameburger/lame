e9a9500aa5422f87cf7cd192aff148936a803283
Lane <72899418+lameburger@users.noreply.github.com>
2023-11-04 11:53:13 -0500

    zdfg

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -16,19 +16,48 @@ function getCadence(){
     console.log("JS loaded")
     DeviceMotionEvent.requestPermission().then(response => {
         if (response == 'granted') {
+            let accelHist = []
+            let avgHist = []
+            let direction = true
+            let timeHist = []
             console.log("accelerometer permission granted");
             window.addEventListener('devicemotion', (event) => {
                 var headingElement = document.getElementById("accel_value");
+                var headingElementEnergy = document.getElementById("energy_value");
                 var netAccel = Math.sqrt(event.acceleration.x ** 2 + event.acceleration.y ** 2 + event.acceleration.z ** 2)
-                
                 let parsed = parseFloat(netAccel.toFixed(3));
+                if (accelHist.length() > 30) {
+                    accelHist.shift()
+                }
+                accelHist.push(netAccel)
+                let avgAccel = accelHist.reduce(adder) / accelHist.length
 
-                setInterval(function() {
-                    var newValue = parsed;
-                    updateChart(newValue);
-                  }, 1000);
+                function adder(total, value, index, array){
+                    return total + value
+                }
 
-                headingElement.textContent = parsed.toString();
+                if (avgHist.length() > 30) {
+                    avgHist.shift()
+                }
+                avgHist.push(avgAccel)
+                if(avgHist[0] < avgHist[avgHist.length - 1] != direction){
+                    direction =  !direction
+                }
+
+                if (timeHist.length() > 5){
+                    timeHist.shift()
+                }
+                timeHist.push(new Date().getTime())
+                let tdiffHist = [0]
+                for(i = 1; i < timeHist.length; i++){
+                    tdiffHist[i] = timeHist[i] - timeHist[i - 1]
+                }
+
+                avgDiff = tdiffHist.reduce(adder) / tdiffHist.length()
+
+                cadence = 60000 / avgDiff
+                
+                headingElementEnergy.textContent = cadence.toString();
             })
         }
         else{
@@ -37,22 +66,12 @@ function getCadence(){
     });
 }
 
-// Update the chart with new data
-function updateChart(newValue) {
-    // Add a new data point to the chart
-    myChart.data.datasets[0].data.push(newValue);
-  
-    // If you want to add labels for each data point (e.g., timestamps)
-    myChart.data.labels.push(new Date().toLocaleTimeString());
-  
-    // Limit the number of data points displayed to a certain number (optional)
-    var maxDataPoints = 20; // Adjust as needed
-    if (myChart.data.datasets[0].data.length > maxDataPoints) {
-      myChart.data.datasets[0].data.shift();
-      myChart.data.labels.shift();
-    }
-  
-    // Update the chart
-    myChart.update();
-  }
-  
+function adjustTempo(tempo, cadence){
+    let possibleTempos = [tempo / 4, tempo / 2, tempo, tempo * 2, tempo * 4]
+    possibleTempos.sort(function(a, b){return Math.abs(a - cadence) - Math.abs(b - cadence)})
+    return possibleTempos[0]
+}
+
+function adjustSpeed(tempo, cadence){
+    return cadence / adjustTempo(tempo, cadence)
+}
