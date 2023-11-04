dab79b4c5a82e9b9c3942dc51e585076abe3e98d
Lane <72899418+lameburger@users.noreply.github.com>
2023-11-04 14:15:57 -0500

    goerge code

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -12,66 +12,76 @@ genreSelect.addEventListener('change', function () {
     genreSelect.classList.add('d-none');
 });
 
-function getCadence(){
-    console.log("JS loaded")
+function getCadence() {
+    console.log("JS loaded");
     DeviceMotionEvent.requestPermission().then(response => {
         if (response == 'granted') {
             let accelHist = []
             let avgHist = []
             let direction = true
             let timeHist = []
+            let longAccelHist = []
             console.log("accelerometer permission granted");
-            window.addEventListener('devicemotion', (event) => {
-                var headingElement = document.getElementById("accel_value");
-                var headingElementEnergy = document.getElementById("energy_value");
-                var netAccel = Math.sqrt(event.acceleration.x ** 2 + event.acceleration.y ** 2 + event.acceleration.z ** 2)
-                let parsed = parseFloat(netAccel.toFixed(3));
-                if (accelHist.length() > 30) {
-                    accelHist.shift()
+            var headingElementEnergy = document.getElementById("energy_value");
+            var headingElement = document.getElementById("accel_value");
+            var headingElementCadence = document.getElementById("cadence_value")
+
+            function processMotionEvent(event) {
+                var netAccel = Math.sqrt(event.acceleration.x ** 2 + event.acceleration.y ** 2 + event.acceleration.z ** 2);
+                let parsed = parseFloat(netAccel.toFixed(1));
+                headingElement.textContent = "Acceleration: " + parsed;
+                
+                //acceleration smoothing
+                if (accelHist.length() > 100) {
+                    accelHist.shift();
                 }
-                accelHist.push(netAccel)
-                let avgAccel = accelHist.reduce(adder) / accelHist.length
+                accelHist.push(netAccel);
+                let avgAccel = accelHist.reduce(adder) / accelHist.length;
 
                 function adder(total, value, index, array){
-                    return total + value
+                    return total + value;
                 }
 
-                if (avgHist.length() > 30) {
-                    avgHist.shift()
+                if (avgHist.length() > 100) {
+                    avgHist.shift();
                 }
                 avgHist.push(avgAccel)
-                if(avgHist[0] < avgHist[avgHist.length - 1] != direction){
-                    direction =  !direction
+                
+                //detect peaks
+                if((avgHist[0] < avgHist[avgHist.length - 1] != direction) && (Math.abs(avgHist[0] - avgHist[avgHist.length - 1]) > 1)){
+                    direction =  !direction;
+                    longAccelHist.push(netAccel)
                 }
-
-                if (timeHist.length() > 5){
-                    timeHist.shift()
+                
+                //finding cadence frequency
+                if (timeHist.length() > 30){
+                    timeHist.shift();
                 }
-                timeHist.push(new Date().getTime())
+                timeHist.push(new Date().getTime());
                 let tdiffHist = [0]
                 for(i = 1; i < timeHist.length; i++){
-                    tdiffHist[i] = timeHist[i] - timeHist[i - 1]
+                    tdiffHist[i] = timeHist[i] - timeHist[i - 1];
                 }
 
-                avgDiff = tdiffHist.reduce(adder) / tdiffHist.length()
+                avgDiff = tdiffHist.reduce(adder) / tdiffHist.length();
 
-                cadence = 60000 / avgDiff
-                
-                headingElementEnergy.textContent = cadence.toString();
-            })
+                cadence = 60000 / avgDiff;
+                headingElementCadence.textContent = "Cadence: " + cadence;
+
+                //Calculate energy
+                if (longAccelHist.length > 30){
+                    longAccelHist.shift();
+                }
+                var energy = cadence * 0.05 * longAccelHist.reduce(adder) / accelHist;
+                headingElementEnergy.textContent = "Energy: " + energy;
+
+                // Set another timeout for the next event processing after 100 milliseconds
+            }
+
+            // Add an event listener for the initial devicemotion event
+            window.addEventListener('devicemotion', processMotionEvent);
+        } else {
+            headingElement.textContent = "Permission not granted";
         }
-        else{
-            headingElement.textContent = "Permission not granted"
-        }
-    });
-}
-
-function adjustTempo(tempo, cadence){
-    let possibleTempos = [tempo / 4, tempo / 2, tempo, tempo * 2, tempo * 4]
-    possibleTempos.sort(function(a, b){return Math.abs(a - cadence) - Math.abs(b - cadence)})
-    return possibleTempos[0]
-}
-
-function adjustSpeed(tempo, cadence){
-    return cadence / adjustTempo(tempo, cadence)
-}
+        });
+    }
