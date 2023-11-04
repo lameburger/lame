c5677aca62d28d15e5da6862cb9f5c4f470074be
Lane <72899418+lameburger@users.noreply.github.com>
2023-11-04 00:36:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let is_running = false;
+
+if (DeviceMotionEvent) {
+    DeviceMotionEvent.requestPermission()
+        .then(response => {
+            if (response === 'granted') {
+                is_running = true
+                console.log('DeviceMotionEvent permission granted.');
+            } else {
+                console.log('DeviceMotionEvent permission denied.');
+            }
+        })
+        .catch(error => {
+            console.error('Error requesting DeviceMotionEvent permission:', error);
+        });
+} else {
+    console.log('DeviceMotionEvent API is not supported on this device.');
+}
+
+function getCadence(){
+    console.log("JS loaded")
+        if (is_running == true) {
+            console.log("accelerometer permission granted");
+            window.addEventListener('devicemotion', (event) => {
+                var headingElement = document.getElementById("accel_val");
+                var netAccel = Math.sqrt(event.acceleration.x ** 2 + event.acceleration.y ** 2 + event.acceleration.z ** 2)
+                
+                window.alert(netAccel)
+                // accelReader.innerHTML = netAccel.toString()
+                headingElement.textContent = netAccel.toString();
+            })
+        }
+        else{
+            accelReader.innerHTML = "JS works!"
+        }
+}
